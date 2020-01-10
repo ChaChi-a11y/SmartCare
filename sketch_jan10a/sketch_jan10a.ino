f7da8671d2e26ba155e01bc4df9f756c9f28b24f
ChaChi-a11y <59736170+ChaChi-a11y@users.noreply.github.com>
2020-01-10 20:09:45 +0600

    Update sketch_jan10a.ino

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -1,60 +1,56 @@
-#ifndef Pins_Arduino_h
-#define Pins_Arduino_h
 
-#include "../generic/common.h"
 
-#define PIN_WIRE_SDA (4)
-#define PIN_WIRE_SCL (5)
 
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
-
-static const uint8_t LED_BUILTIN = 16;
-static const uint8_t BUILTIN_LED = 16;
-
-static const uint8_t D0   = 16;
-static const uint8_t D1   = 5;
-static const uint8_t D2   = 4;
-static const uint8_t D3   = 0;
-static const uint8_t D4   = 2;
-static const uint8_t D5   = 14;
-static const uint8_t D6   = 12;
-static const uint8_t D7   = 13;
-static const uint8_t D8   = 15;
-static const uint8_t D9   = 3;
-static const uint8_t D10  = 1;
-
-#endif /* Pins_Arduino_h */
-
-const int keysPin[4] = {
-  16,5,4,0
-}
-const int dataPin[4] = {
-  14,12,13,15
-}
-static const int pot[2] = {
-  3,1
-}
-char **comm = new char[4];
-namespace request{
-  void dht();
-  void net();
-  void loadSD();
-  void chooser();
-}
-namespace out{
-  void lcd();
-  void net();
-  void saveSD();
-  void relay();
-}
+#include <SPI.h>
+#include <SD.h>
+const int PIN_CHIP_SELECT = 4;
 void setup() {
   // put your setup code here, to run once:
-  for(int i = 0; i < 4; i++)
-    comm[i] = new char;
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+ // Этот пин обязательно должен быть определен как OUTPUT
+  pinMode(10, OUTPUT);
+  // Пытаемся проинициализировать модуль
+  if (!SD.begin(PIN_CHIP_SELECT)) {
+    Serial.println("Card failed, or not present");
+    // Если что-то пошло не так, завершаем работу:
+    return;
+  }
+  Serial.println("card initialized.");
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+// Строка с данными, которые мы поместим в файл:
+  String logStringData = "";
+  // Считываем данные с портов и записываем в строку
+  for (int i = 0; i < 5; i++) {
+    int sensor = analogRead(i);
+    logStringData += String(sensor);
+    if (i < 4) {
+      logStringData += ",";
+    }
+  }
+  // Открываем файл, но помним, что одновременно можно работать только с одним файлом.
+  // Если файла с таким именем не будет, ардуино создаст его.
+  File dataFile = SD.open("datalog.csv", FILE_WRITE);
+  // Если все хорошо, то записываем строку:
+  if (dataFile) {
+    dataFile.println(logStringData);
+    dataFile.close();
+    // Публикуем в мониторе порта для отладки
+    Serial.println(logStringData);
+  }
+  else {
+  // Сообщаем об ошибке, если все плохо
+    Serial.println("error opening datalog.csv");
+  }
 }
+
+
+
+
+
